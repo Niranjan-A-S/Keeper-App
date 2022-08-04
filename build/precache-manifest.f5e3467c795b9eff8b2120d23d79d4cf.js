0ec878803a3e0e85c987db6935b8a90751b87ebd
Niranjan <niranjan0881@gmail.com>
2022-08-04 23:17:30 +0530

    initial commit

---
+++ build/precache-manifest.f5e3467c795b9eff8b2120d23d79d4cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2193b34b50fb3709cfcfd3f6ea030e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df96382a62fbddfefea",
+    "url": "/static/js/2.e95a7df2.chunk.js"
+  },
+  {
+    "revision": "03fe4c1d69ebd6dd88b7",
+    "url": "/static/js/main.64817139.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
